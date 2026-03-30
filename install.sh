66e2f4e2968d0105567cc95bdbe4d754a9854b6b
Stephan Boyer <stephan@stephanboyer.com>
2026-03-29 21:08:32 -0700

    Install v0.0.8 by default

--- install.sh
+++ install.sh
@@ -14,7 +14,7 @@
   DESTINATION="${PREFIX:-/usr/local/bin}/shell-agent"
 
   # Which version to download
-  RELEASE="v${VERSION:-0.0.7}"
+  RELEASE="v${VERSION:-0.0.8}"
 
   # Determine which binary to download.
   FILENAME=''
